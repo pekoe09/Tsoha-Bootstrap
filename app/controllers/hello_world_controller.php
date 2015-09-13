ce75dc55c00b28d5bb157b0dae331c5262f594e3
Juha Kangas <juha.kangas@welho.com>
2015-09-13 21:39:29 +0300

    Sivujen muokkausta

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -3,8 +3,7 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  echo 'Tämä on etusivu';
+      View::make('suunnitelmat/etusivu.html');
     }
 
     public static function sandbox(){
